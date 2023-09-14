81b79bf338755d14661655fdea03fe4061f98291
ElianGuerrero <145053455+ElianGuerrero@users.noreply.github.com>
2023-09-14 12:58:25 -0600

    Create Degreescalculator.py

---
+++ Degreescalculator.py
@@ -0,0 +1,12 @@
+
+Degrees calculator: 
+def f(x):
+  f_g = int(x)
+  return f_g
+
+print("Give me the degrees in celcius and I will give you the degrees in Farenheit and Kelvin")
+x = input()
+
+g1 = int(x)*float(1.8)+32
+g2 = int(x)+273.15
+print("Your answer is",g1,"and",g2)
