3c48ffce587dc95327f7ec2deb4cc306f7896526
ElianGuerrero <145053455+ElianGuerrero@users.noreply.github.com>
2023-09-14 12:57:15 -0600

    Create Codingrush.py

---
+++ Codingrush.py
@@ -0,0 +1,10 @@
+
+Coding rush:
+def name():
+  print("What is your name?")
+  variable = input( )
+  print("Hello my friend", variable)
+  print(Give me a number and I will give you the number elevated to the square")
+  number = input( )
+  result = int(number)**(1/2)
+  print("Your number, elevated to the square is", result)
